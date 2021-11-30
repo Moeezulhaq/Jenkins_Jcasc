7c855c0a2c400276a768c1e952b9571aaff5f570
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-30 10:20:41 -0800

    added seed files

--- seed.groovy
+++
@@ -1,8 +0,0 @@
-job("seed") {
-    if (binding.hasVariable('RUN_AFTER_BOOT')) { // Only true when creating a new jenkins instance or reloading config
-        triggers {
-            cron("H/2 * * * *")
-        }
-    }
-    ......
-}
