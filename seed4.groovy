4f717f5c77623fe0ca065e7646f4c21302dd1d7f
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-12-01 10:50:29 -0800

    final code

--- seed4.groovy
+++ seed4.groovy
@@ -25,3 +25,73 @@ pipelineJob('second pipeline') {
         }
     }
 }
+
+pipelineJob('3 pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('Moeezulhaq/Jenkins_test')
+                    }
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('4 pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('Moeezulhaq/Jenkins_test')
+                    }
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('5 pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('Moeezulhaq/Jenkins_test')
+                    }
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('6 pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('Moeezulhaq/Jenkins_test')
+                    }
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('7 pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('Moeezulhaq/Jenkins_test')
+                    }
+                }
+            }
+        }
+    }
+}
